9d839e18f8809ba8b174720147cdca8b6d12d33c
Marvin Johanning <manimusmuellus@gmail.com>
2021-11-28 16:29:41 +0100

    Modify header navigation

--- blog/2021/header.php
+++ blog/2021/header.php
@@ -2,6 +2,8 @@
   <li class="header"><a href="/index.php">Agora</a></li>
   <li class="header"><a href="/contact.php">Contact</a></li>
   <li class="header"><a href="/about.php">About</a></li>
+  <li class="header"><a href="/updates/updates.php">Updates</a></li>
+  <li class="header"><a href="/projects.php">Current projects</a></li>
   <br>
   <li class="header"><a href="/blog/index.php">Blog — Overview</a> </li>
   <li class="header"><a href="/blog/2021/index.php">Blog — 2021</a></li>
--- shelf/virtual/header.php
+++ shelf/virtual/header.php
@@ -2,6 +2,8 @@
   <li class="header"><a href="/index.php">Agora</a></li>
   <li class="header"><a href="/contact.php">Contact</a></li>
   <li class="header"><a href="/about.php">About</a></li>
+  <li class="header"><a href="/updates/updates.php">Updates</a></li>
+  <li class="header"><a href="/projects.php">Current projects</a></li>
   <br>
   <li class="header"><a href="/shelf/virtual/index.php">Virtual Shelf — Overview</a></li>
 </ul>
