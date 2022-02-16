b19be83588af63c2c0bdcd051753c6c66e53a687
Marvin Johanning <manimusmuellus@gmail.com>
2022-02-16 21:14:33 +0100

    Add WIP of Greek pronunciation

---
+++ greekgeneral/greek_pronunciation.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="description"
+    content="An article on how to pronounce the Ancient Greek language.">
+  <title>How to pronounce Ancient Greek?</title>
+
+  <link rel="preload" as="style" href="/CSS/styles.css">
+  <link rel="stylesheet" href="/CSS/styles.css">
+  <script defer data-domain="ancient-greek.net" src="https://analytics.ancient-greek.net/js/plausible.js"></script>
+
+  <?php include('header.php'); ?>
+</head>
+
+<div class="heading-greek">
+  <h1>How to pronounce Ancient Greek?</h1>
+  <h3>Τὴν ἀρχαίαν Ἑλληνικὴν γλῶτταν πῶς φθέγγῃ;</h3>
+  <i>How should you pronounce the Ancient Greek language?</i>
+</div>
+
+<body>
+  <div class="article">
+    <p>Learning the pronunciation of an ancient language does not appear to be a priority for most people, and it is, at least somewhat, understandable — why would you want to, seemingly, waste time learning something that you will likely not use much again thereafter; and even if you <i>do</i> use it, who is going to judge your pronunciation? Native speakers of the language? Surely that is not going to be the case. You may, therefore, find it surprising that I value learning even an ancient language’s pronunciation at least somewhat properly; and precisely because of that, I shall be discussing the various ways of pronuncing Ancient Greek, how I personally pronounce it and I shall also more thoroughly explain the reason behind my finding the learning of a dead language’s pronunciation important — and it might not even be as dead as you think.</p>
+  </div>
+
+  <div class="article">
+    <h2>The importance of pronunciation</h2>
+    <p>First, it is important to realise that language has gained a written form only comparatively recently. The first forms of actual writing that we know of date to around 5000 years in the past, namely either Ancient Egyptian or Sumerian<a class="footnote" href="#footnote_1" id="footnote_1_ref">Note [1]</a> — depending on whom you ask. It is simply naïve to believe that language did not exist before that time and that both written <i>and</i> spoken language simply appeared out of nowhere on the exact same date.</p>
+    
+    <p>Instead, human language is, in its essence, a verbal form of communication. Only recently have we begun to associate the <i>knowing</i> of a language with being able to write and read in it. This associating of writing with actual language has now, apparently, gone so far as to make people believe writing and language have coexisted forever; for I have, indeed, met two people who have expressed the opinion that they did, in fact, always exist simultaneously. It was entirely incomprehensible to them how the concept of <q>language</q> could exist without writing.<a class="footnote" href="#footnote_2" id="footnote_2_ref">Note [2]</a></p>
+    
+    <p>Back in the days of the Ancient Greeks (or the people who lived at the time of the New Testament) — which is, of course, what this site concerns itself with —, it was, however, most uncommon to find someone who was actually literate, especially if they were not from a large city such as Athens and of a high social status. There was simply no need for the majority of the populace to have the ability to read; people shared their knowledge and wisdom by word of mouth only for a large amount of time; indeed, even Homer’s poetic epics are largely told and retold orally before finally being written down.</p>
+
+    <p>This oral recitation of literature also reveals why rhymes (or other methods, such as the metre found in Homeric poems) are such a common trait amongst very old pieces of literature — things that rhyme are, simply, easier to remember. It is, therefore, of the utmost importance to be able to properly pronounce words if things such as poems are to be recited properly.</p>
+
+    <p>In addition to the above-mentioned reasons, Ancient Greek is still being spoken — albeit in a changed form called Modern Greek, or simply Greek. If one is to learn a languge whose descendant is still alive and well, should one not make an effort to properly reconstruct its sound and pronounce it the way it was pronounced 2,000 years ago? Or one might simply superimpose the pronunciation of the modern variety on to the ancient one, as many people in Greece themselves — when they have to learn Ancient Greek at school — do.</p>
+
+  </div>
+
+  <div class="article">
+    <h2>Problems with what is commonly taught</h2>
+    <p>Unfortunately, the commonly taught so-called Erasmian pronunciation (which received its name from its inventor, the Dutch theologian and philospher Erasmus of Rotterdam) is nothing of the sort, as it is filled with anachronisms that turn it into a monster which would not have existed at any point of time during the language’s natural life. Pupils are not encouraged to break from their native tongue’s phonology, leading to many varieties of ear torture.</p>
+      
+    <p>Indeed, the worst offender I have, as of today, seen was a Bible app I found for my phone. It is a great app and I use it frequently and it consists of videos of Bible passages being read and <q>dissected</q> so that they are easier to comprehend; the presenter is, no doubt, much more proficient in the language than I am, but his pronunciation made me unable to enjoy watching his videos for too long. He is pronouncing the words as if they were English and his American English accent is so pronounced — a pun may or may not be intended —, it is actually <i>difficult</i> for me to make out the words he is saying. </p>
+
+    <p>I am not here to make fun of people’s pronunciation and the above-mentioned person will, therefore, remain without a name. I simply mentioned it to showcase how even someone whose knowledge of Greek is far superior to mine can have a pronunciation that is worse than any that I have heard since or before.</p>
+
+    <p>I guess I am privileged in that I have always been able to learn a language’s pronunciation with relative ease. My English sounds positively British, my Swedish sounds rather Stockholm-y and my Russian has convinced several people that I have a Russian background — despite my Russian being limited to a few dozen sentences. It is certainly difficult and unusual in the beginning, but after a few weeks and months it generally becomes second nature to me. I am aware that not everyone has this ability to learn a new pronunciation with ease, but I do find that a large portion of the population does not put a lot of effort into perfecting their pronunciation of a foreign language: <q>I can be understood well enough, why should I improve?</q></p>
+
+    <p>Let us, however, continue and explore the various ways of pronouncing Ancient Greek which I personally believe to be the most eloquent methods of doing so. Others may — and are, certainly, allowed to — disagree with me and the definition of <q>eloquent</q> is rather subjective, too. Therefore, please be aware that everything I will discuss below reflects my personal opinion and is, in no way supposed to reflect the most <q>ideal</q> or <q>correct</q> method of doing it. I am simply proposing them as an improvement over the current system.</p>
+  </div>
+
+  <div class="footnote">
+    <h2>Footnotes</h2>
+    <p id="footnote_1">[1] If you are interested in Ancient Egyptian hieroglyphics, I would like to recommend my book to you, The Intricacies of Ancient Egyptian Hieroglpyhics. Additionally, if you are interested in learning more about Sumerian, I cannot recommend Irving Finkel’s lectures and talks enough. They are delightful to watch not only because of the interesting content but also because of the often rather humerous speaker. <a href="#footnote_1_ref">Back to text</a></p>
+
+    <p id="footnote_2">[2] This reminds me of a somewhat amusing anecdote I thought fitting to share here. A while ago, whilst talking to one of my colleagues at work, the topic of beer was brought up in our conversation. What exactly prompted this I do not remember, but I distinctly remember his saying that Germans had invented beer. I remarked, thereupon, that even the Ancient Egyptians enjoyed their own version of a pint, he was struck with disbelief and proclaimed that surely their <q>beer</q> cannot have been real beer at all — for it was us, the Germans, who have invented it! This was followed by my slowly nodding in fake agreement to move the conversation further. <a href="#footnote_2_ref">Back to text</a></p>
+  </div>
+</body>
+
+<footer>
+  <?php include($_SERVER['DOCUMENT_ROOT'].'/footer.php'); ?>
+</html>
